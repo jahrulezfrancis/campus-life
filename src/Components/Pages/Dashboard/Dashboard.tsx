f37ba8b68538496697fe239e4a995bdf7d2f7521
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-08 22:53:39 +0000

    Adding content

--- src/Components/Pages/Dashboard/Dashboard.tsx
+++ src/Components/Pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,14 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
+import UnverifiedMessage from "../../Sections/Unverified";
+import AlmanacGenerator from "../../Core/AlmanacGenerator/AlamacGenerator";
 
 export default function Dashboard() {
+    const isVerified = false
     return (
-        <Box>
-            This is your Dashboard
+        <Box height="300px">
+            <Heading>Hi Isa'ac</Heading>
+            {!isVerified && <UnverifiedMessage />}
+            {!isVerified && <AlmanacGenerator />}
         </Box>
     )
 }
