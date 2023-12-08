fcc1bc36778990f6935a95563788a7c83c557734
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-08 14:41:00 +0000

    Initial commit

---
+++ src/Components/Pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Dashboard() {
+    return (
+        <Box>
+            This is your Dashboard
+        </Box>
+    )
+}
---
+++ src/Components/Pages/LandingPage/Homepage.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function HomePage() {
+    return (
+        <Box>
+            You HomePage
+        </Box>
+    )
+}
