fbb4eed6ccc98adb677227984640bf5a993a1979
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-09 01:19:27 +0000

    Removed almanac generator from Dashboard

--- src/Components/Pages/Dashboard/Dashboard.tsx
+++ src/Components/Pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,12 @@
 import { Box, Heading } from "@chakra-ui/react";
 import UnverifiedMessage from "../../Sections/Unverified";
-import AlmanacGenerator from "../../Core/AlmanacGenerator/AlamacGenerator";
 
 export default function Dashboard() {
     const isVerified = false
     return (
-        <Box height="300px">
+        <Box padding={5} height="300px">
             <Heading>Hi Isa'ac</Heading>
             {!isVerified && <UnverifiedMessage />}
-            {!isVerified && <AlmanacGenerator />}
         </Box>
     )
 }
