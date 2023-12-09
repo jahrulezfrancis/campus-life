ccb3269d203613536252fad706bee2c3e397aa04
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-09 01:17:43 +0000

    Updating routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './Components/Pages/LandingPage/Homepage'
 import Dashboard from './Components/Pages/Dashboard/Dashboard'
 import NavBar from './Components/Navigation/NavBar'
+import ProfilePage from './Components/Pages/Profile/Profile'
+import UploadsPage from './Components/Pages/Uploads/Uploads'
+import SignUpPage from './Components/Pages/SignUp/SignUpPage'
 
 function App() {
 
@@ -12,6 +15,10 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/my-uploads' element={<UploadsPage />} />
+        <Route path='/sign-up' element={<SignUpPage />} />
+        <Route path='/my-uploads' element={<UploadsPage />} />
       </Routes>
     </Box>
   )
