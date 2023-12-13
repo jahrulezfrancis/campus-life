eadb5c60dbba9e750ce6cd09f92d41b32e0fcb79
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-13 03:57:05 +0000

    Added cards section

--- src/Components/Pages/Dashboard/Dashboard.tsx
+++ src/Components/Pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,7 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, HStack, Heading } from "@chakra-ui/react";
 import UnverifiedMessage from "../../Sections/Unverified";
+import PhotoHighlightCard from "../../Sections/Cards/ImageHighlight";
+import SocialMediaCard from "../../Sections/Cards/Social";
 
 export default function Dashboard() {
     const isVerified = false
@@ -7,6 +9,12 @@ export default function Dashboard() {
         <Box padding={5} height="300px">
             <Heading>Hi Isa'ac</Heading>
             {!isVerified && <UnverifiedMessage />}
+            <section>
+                <HStack p={5} gap={10} wrap="wrap">
+                    <PhotoHighlightCard />
+                    <SocialMediaCard />
+                </HStack>
+            </section>
         </Box>
     )
 }
