971ea3ecac6b68a6dfa2cb381e50a59e9c9e3328
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-13 03:55:44 +0000

    Initial commit

---
+++ src/Components/Sections/Cards/SchoolCalender.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import PreviewCardLayout from "./CardLayout";
+
+export default function SchoolCalender(){
+    
+    return(
+        <Box>
+            <PreviewCardLayout bgImage={""} cardLabel="View Semester calender" children={
+                <>
+
+                </>
+            } />
+        </Box>
+    )
+}
