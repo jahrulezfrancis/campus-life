095ff878e670dbc24e874fd032f9f0f9edf5a8d5
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-13 06:33:30 +0000

    Implementing mobile adaptability

--- src/Components/Sections/Cards/CardLayout.tsx
+++ src/Components/Sections/Cards/CardLayout.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Heading } from "@chakra-ui/react";
+import { Box, Stack, Heading, useMediaQuery } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface CardProps {
@@ -8,10 +8,11 @@ interface CardProps {
 }
 
 export default function PreviewCardLayout(props: CardProps) {
+    const [mobileDevice] = useMediaQuery("(max-width: 600px)")
     return (
-        <Box borderRadius={8} bgImage={props.bgImage} backgroundRepeat="no-repeat" backgroundPosition="center" backgroundSize="cover" width={300} height={300}>
-            <Stack borderRadius={8} bg="rgba(0, 0, 0, 0.5)" height="100%" align="center" justify="center">
-            <Heading color="#E2E8F0" as="h3" textAlign="center">{props.cardLabel}</Heading>
+        <Box borderRadius={8} bgImage={props.bgImage} backgroundRepeat="no-repeat" backgroundPosition="center" backgroundSize="cover" width={mobileDevice ? "300px" : 400} height={300}>
+            <Stack borderRadius={8} bg="rgba(0, 0, 0, 0.5)" height="100%" width="100%" align="center" justify="center">
+                <Heading color="#E2E8F0" as="h3" textAlign="center">{props.cardLabel}</Heading>
                 {props.children}
             </Stack>
         </Box>
