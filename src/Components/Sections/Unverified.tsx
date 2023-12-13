e29f723490292fb5b9bb743ccf3b37ca35a7f6cf
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-13 06:32:49 +0000

    Updating verification card

--- src/Components/Sections/Unverified.tsx
+++ src/Components/Sections/Unverified.tsx
@@ -1,15 +1,18 @@
-import { Box, Center, Heading, Stack, Text } from "@chakra-ui/react";
+import { Center, Heading, Stack, Text, useMediaQuery } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 export default function UnverifiedMessage() {
+    const [mobileDevice] = useMediaQuery("(max-width: 600px)")
+
+
     return (
         <Center>
-            <Box borderRadius={8} alignItems="center" justifyContent="center" background="#FEB74D" width="70vw" height={200}>
-                <Stack height="100%" align="center" justify="center">
-                    <Heading>Your account is yet to be verified</Heading>
-                    <Text>Hello Isa'ac, You account is not yet verified, <Link to="/check-point"><u>click here to verify your account</u></Link></Text>
+            <Stack borderRadius={8} align="center" justify="center" background="#FEB74D" maxW={mobileDevice ? "85vw" : "70vw"} minW={mobileDevice ? "80vw" : "70vw"} minH={200}>
+                <Stack p={5} height="100%" align="center" justify="center">
+                    <Heading>No uploads for this semester yet</Heading>
+                    <Text>Hello Isa'ac, you are yet to upload your picture(s) for this semester, <Link to="/my-uploads"><u>click here to go to the uploads page</u></Link></Text>
                 </Stack>
-            </Box>
+            </Stack>
         </Center>
     )
 }
