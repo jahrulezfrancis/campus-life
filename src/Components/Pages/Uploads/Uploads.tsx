a8cc12b65fa647ab81f948f0b68041b5c00a586f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-14 22:09:48 +0000

    Updating upload page layout and content

--- src/Components/Pages/Uploads/Uploads.tsx
+++ src/Components/Pages/Uploads/Uploads.tsx
@@ -1,10 +1,28 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Heading, Stack, Text } from "@chakra-ui/react"
 import FileUploader from "../../Core/Helpers/FileUploader"
+import { primaryColor } from "../../Core/Colors/colors"
+import Carousel from "../../Sections/Slider/Carousel"
 
 export default function UploadsPage() {
+    const fileLength = 0
     return (
         <Box>
-         <FileUploader />
+            <section>
+                <Carousel />
+            </section>
+            <section>
+                <Stack justify="center" align="center" width="auto" p={10} m={10} border={`1px dashed ${primaryColor}`}>
+                    {fileLength < 1 && <Text color="red">You have not uploaded any picture yet</Text>}
+                    <FileUploader />
+                </Stack>
+            </section>
+            <section>
+                <Box>
+                    <Stack>
+                        <Heading as="h3"></Heading>
+                    </Stack>
+                </Box>
+            </section>
         </Box>
     )
 }
