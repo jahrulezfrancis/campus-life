9f1a5bd2e4ca8022508f7369aea9a70eea99098d
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-14 22:10:18 +0000

    Changed color source

--- src/Components/Sections/Unverified.tsx
+++ src/Components/Sections/Unverified.tsx
@@ -1,5 +1,6 @@
 import { Center, Heading, Stack, Text, useMediaQuery } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { warningColor } from "../Core/Colors/colors";
 
 export default function UnverifiedMessage() {
     const [mobileDevice] = useMediaQuery("(max-width: 600px)")
@@ -7,7 +8,7 @@ export default function UnverifiedMessage() {
 
     return (
         <Center>
-            <Stack borderRadius={8} align="center" justify="center" background="#FEB74D" maxW={mobileDevice ? "85vw" : "70vw"} minW={mobileDevice ? "80vw" : "70vw"} minH={200}>
+            <Stack borderRadius={8} align="center" justify="center" background={warningColor} maxW={mobileDevice ? "85vw" : "70vw"} minW={mobileDevice ? "80vw" : "70vw"} minH={200}>
                 <Stack p={5} height="100%" align="center" justify="center">
                     <Heading>No uploads for this semester yet</Heading>
                     <Text>Hello Isa'ac, you are yet to upload your picture(s) for this semester, <Link to="/my-uploads"><u>click here to go to the uploads page</u></Link></Text>
