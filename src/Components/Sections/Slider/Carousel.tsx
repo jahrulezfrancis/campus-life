c5116bc7f94f6409b280135aa6ca0b1ac8c4bd2e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-15 16:13:17 +0000

    Implenting carousel slider for hightlights

--- src/Components/Sections/Slider/Carousel.tsx
+++ src/Components/Sections/Slider/Carousel.tsx
@@ -1,9 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import Imagebg from "../../../assets/calender_image.png"
+import { Box, Image } from "@chakra-ui/react"
 
-export default function Carousel() {
+export default function HighlightCarousel() {
     return (
-        <Box>
-            There you have it the carousel
-        </Box>
-    )
+        <Carousel swipeable stopOnHover infiniteLoop width="100%" autoPlay>
+            <Box>
+                <Image objectFit="cover" h="600px" src={Imagebg} />
+                {/* <p className="legend">Memories from year one</p> */}
+            </Box>
+            <Box  >
+                <Image objectFit="cover" h="600px" src={Imagebg} />
+                {/* <p className="legend">Highlights from year two</p> */}
+            </Box>
+            <Box>
+                <Image objectFit="cover" h="600px" src={Imagebg} />
+                {/* <p className="legend">Highlights from year three</p> */}
+            </Box>
+            <Box>
+                <Image objectFit="cover" h="600px" src={Imagebg} />
+                {/* <p className="legend">Highlights from year four</p> */}
+            </Box>
+            <Box>
+                <Image objectFit="cover" h="600px" src={Imagebg} />
+                {/* <p className="legend">Highlights from year five</p> */}
+            </Box>
+        </Carousel>
+    );
 }
