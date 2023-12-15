228d94f266c8bc4504bf7bff98add96440c54ddb
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-15 10:26:43 +0000

    Intial commit

---
+++ src/Components/Sections/Cards/YearlyData.ts
@@ -0,0 +1,7 @@
+export const yearlyData = [
+    { id: 1, title: "Year one", description: "View year one hiighlight", buttonLabel: "Explore Year one" },
+    { id: 2, title: "Year Two", description: "View year two highlights", buttonLabel: "Explore Year two" },
+    { id: 3, title: "Year Three", description: "View year three highlights", buttonLabel: "Explore year three" },
+    { id: 2, title: "Year four", description: "View year four highlights", buttonLabel: "Explore Year four" },
+    { id: 2, title: "Year five", description: "View year five highlights", buttonLabel: "Explore Year five" },
+]
