0a4a9ab7dc2aacf89b3a12649a2c9ca44969aaf2
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-16 13:32:05 +0000

    Changed Dashboard icon

--- src/Components/Navigation/navitems.ts
+++ src/Components/Navigation/navitems.ts
@@ -1,8 +1,9 @@
-import { CgProfile, CgBorderStyleDashed } from "react-icons/cg"
+import { CgProfile } from "react-icons/cg"
+import { MdDashboard } from "react-icons/md";
 import { IoCloudUploadSharp } from "react-icons/io5";
 
 export const navItems = [
-    { id: 1, path: "dashboard", title: "Dashboard", icon: CgBorderStyleDashed },
+    { id: 1, path: "dashboard", title: "Dashboard", icon: MdDashboard },
     { id: 2, path: "profile", title: "Profile", icon: CgProfile },
     { id: 3, path: "my-uploads", title: "My Uploads", icon: IoCloudUploadSharp },
 ]
