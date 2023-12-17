60b7799ec02a87bd7241a4066981767d77d3c929
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-17 21:54:12 +0000

    Improving adaptability

--- src/Components/Pages/LandingPage/AboutSection.tsx
+++ src/Components/Pages/LandingPage/AboutSection.tsx
@@ -1,21 +1,24 @@
-import { Box, HStack, Heading, Stack } from "@chakra-ui/react";
+import { Box, HStack, Heading, Stack, Image, Text, useMediaQuery } from "@chakra-ui/react";
+import StudentPicture from "../../../assets/black-female-student.jpg"
 
 export default function AboutSection() {
+    const [isMobile] = useMediaQuery("(max-width: 650px)")
     return (
-        <Box>
-            <HStack>
+        <Box p={10}>
+            <HStack wrap="wrap" gap={10} justify="center" align="center">
                 <Stack>
-                    <Heading>About Campuslife</Heading>
+                    <Heading width={isMobile ? "300px" : "500px"} fontSize={isMobile ? 50 : 80}>About Campuslife</Heading>
+                    <Image maxW={600} width="300px" src={StudentPicture} />
                 </Stack>
-                <Stack>
-                    <Heading>
+                <Stack wrap="wrap" gap={10} align="center" width={isMobile ? "auto" : "600px"} fontWeight={700}>
+                    <Text color="black" fontSize={30} textAlign="start">
                         An innovative campus life record system is envisioned as a comprehensive digital platform designed to empower students, enhance their academic journey, and foster a vibrant campus community.
-                    </Heading>
-                    <Heading>
+                    </Text>
+                    <Text fontSize={30}>
                         This multifaceted system serves as a dynamic hub where students can document their achievements, skills, and experiences while seamlessly connecting with peers and faculty members.
-                    </Heading>
+                    </Text>
                 </Stack>
             </HStack>
-        </Box >
+        </Box>
     )
 }
--- src/Components/Pages/LandingPage/Header.tsx
+++ src/Components/Pages/LandingPage/Header.tsx
@@ -4,17 +4,17 @@ import HeaderImage from "../../../assets/thirrdStudent.jpg"
 
 export default function LandingPageHeader() {
     return (
-        <Box bg="#004138" height={600}>
+        <Box bg="#004138" minH={600} height={"auto"} p={10}>
             <HStack justify="center" align="center" wrap="wrap">
                 <Box padding={5}>
-                    <Image height={550} src={HeaderImage} alt="Header image" />
+                    <Image width="auto" objectFit="cover" height={550} src={HeaderImage} alt="Header image" />
                 </Box>
-                <Stack justify="center" align="center" color="white">
-                    <Heading textTransform="uppercase" fontSize="60px">Welcome to <br /> campus life</Heading>
-                    <Heading mt={15} as="h4" fontSize={22}>Crafting memories</Heading>
-                    <Heading as="h4" fontSize={22}>Building a future</Heading>
+                <Stack wrap="wrap" justify="center" align="center" color="white">
+                    <Heading textAlign="center" textTransform="uppercase" fontSize="60px">Welcome to <br /> campus life</Heading>
+                    <Heading mt={15} fontSize={22}>Crafting memories</Heading>
+                    <Heading fontSize={22}>Building a future</Heading>
                     <Spacer mb={50} height={50} />
-                    <HStack gap={10} wrap="wrap">
+                    <HStack justify="center" gap={10} wrap="wrap">
                         <SolidButton buttonText="Sign in" />
                         <SolidButton buttonText="Sign up" />
                     </HStack>
