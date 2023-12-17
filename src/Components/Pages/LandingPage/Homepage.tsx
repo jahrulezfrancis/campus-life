b017e253af59b49be6cab5b38b2a89c7e2bf7646
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-17 16:39:15 +0000

    Updating Homepage

--- src/Components/Pages/LandingPage/Homepage.tsx
+++ src/Components/Pages/LandingPage/Homepage.tsx
@@ -1,9 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import LandingPageHeader from "./Header";
+import AboutSection from "./AboutSection";
 
 export default function HomePage() {
     return (
         <Box>
-            You HomePage
+            <LandingPageHeader />
+            <section>
+                <Heading as="h3">The road map to a life <br /> long connected Alumni.</Heading>
+                <Heading>It begins<strong>here___</strong></Heading>
+            </section>
+            <section>
+                <AboutSection />
+            </section>
+            <section>
+                <Box bg="#000000" h={"700px"}>
+                    <Text>Who campus life is for</Text>
+                </Box>
+            </section>
         </Box>
     )
 }
