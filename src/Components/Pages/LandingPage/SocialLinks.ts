7106a4cb2d3878ab9d2d4aecd76f64551534f118
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-18 22:01:30 +0000

    Initial commit

---
+++ src/Components/Pages/LandingPage/SocialLinks.ts
@@ -0,0 +1,10 @@
+import { FiInstagram } from "react-icons/fi"
+import { TfiFacebook } from "react-icons/tfi"
+import { FaTwitter } from "react-icons/fa"
+
+
+export const SocialLinks = [
+    { id: 1, name: "Instagram", link: "#", Icon: FiInstagram },
+    { id: 2, name: "Facebook", link: "#", Icon: TfiFacebook },
+    { id: 3, name: "Twitter", link: "#", Icon: FaTwitter },
+]
