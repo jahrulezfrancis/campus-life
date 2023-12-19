eb0774897ad27e95d9ccd78abc558a14aa302d5a
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-19 06:26:55 +0000

    Replaced header image

--- src/Components/Pages/LandingPage/Header.tsx
+++ src/Components/Pages/LandingPage/Header.tsx
@@ -1,14 +1,15 @@
-import { Box, HStack, Heading, Image, Spacer, Stack } from "@chakra-ui/react";
+import { Box, HStack, Heading, Spacer, Stack } from "@chakra-ui/react";
 import SolidButton from "../../Sections/Buttons/SolidButton";
-import HeaderImage from "../../../assets/thirrdStudent.jpg"
+// import HeaderImage from "../../../assets/thirrdStudent.jpg"
+import BackgroundImage from "../../../assets/graduating-students-atbu.jpg"
 
 export default function LandingPageHeader() {
     return (
-        <Box bg="#004138" minH={600} height={"auto"} p={10}>
-            <HStack justify="center" align="center" wrap="wrap">
-                <Box padding={5}>
+        <Box bgSize="cover" bgImage={BackgroundImage} h={"600px"} minH={600}>
+            <HStack height="100%" bg="rgba(0, 0, 10, 0.5)" justify="center" align="center" wrap="wrap">
+                {/* <Box padding={5}>
                     <Image width="auto" objectFit="cover" height={550} src={HeaderImage} alt="Header image" />
-                </Box>
+                </Box> */}
                 <Stack wrap="wrap" justify="center" align="center" color="white">
                     <Heading textAlign="center" textTransform="uppercase" fontSize="60px">Welcome to <br /> campus life</Heading>
                     <Heading mt={15} fontSize={22}>Crafting memories</Heading>
