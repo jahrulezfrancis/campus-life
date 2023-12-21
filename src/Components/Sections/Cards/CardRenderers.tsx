9d94f6d2a5bbed0fc55921ef097f2d204ea1c99c
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-21 09:14:41 +0000

    Adding links to card buttons

--- src/Components/Sections/Cards/CardRenderers.tsx
+++ src/Components/Sections/Cards/CardRenderers.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { yearlyData } from "./YearlyData";
 import PreviewCardLayout from "./CardLayout";
 import SocialMediaCard from "./SocialCardComp";
+import { Link } from "react-router-dom";
 
 export default function CardRenderers() {
     return (
@@ -9,10 +10,12 @@ export default function CardRenderers() {
             <Flex justify="center" gap={10} wrap="wrap">
                 {yearlyData.map((item) => {
                     return (
-                        <PreviewCardLayout bgImage={item.cardImage} cardLabel={item.title} children={
+                        <PreviewCardLayout key={item.id} bgImage={item.cardImage} cardLabel={item.title} children={
                             <>
                                 <Text color="whitesmoke">{item.description}</Text>
-                                <Button>Explore {item.title} highlights</Button>
+                                <Link to={"/" + item.link}>
+                                    <Button>Explore {item.title} highlights</Button>
+                                </Link>
                             </>
                         } />
                     )
