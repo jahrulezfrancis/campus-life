a83d785d5db5c1a6c92cf3714d55322f62271306
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-12-21 09:15:07 +0000

    Updating adding link to data

--- src/Components/Sections/Cards/YearlyData.ts
+++ src/Components/Sections/Cards/YearlyData.ts
@@ -5,9 +5,9 @@ import yearFourPic from "../../../assets/student-in-lab.jpg"
 import finalYearPic from "../../../assets/student2.jpeg"
 
 export const yearlyData = [
-    { id: 1, title: "Year one", description: "Check out your collections from year one", buttonLabel: "Explore Year one", cardImage: yearOnePic },
-    { id: 2, title: "Year Two", description: "Check out your collections from year two", buttonLabel: "Explore Year two", cardImage: yearTwopic },
-    { id: 3, title: "Year Three", description: "Checkout your highlights from year three", buttonLabel: "Explore year three", cardImage: yearthreePic },
-    { id: 2, title: "Year four", description: "Checkout your highlights from year four", buttonLabel: "Explore Year four", cardImage: yearFourPic },
-    { id: 2, title: "Year five", description: "Checkout your highlights from year five", buttonLabel: "Explore Year five", cardImage: finalYearPic },
+    { id: 1, link: "year-one", title: "Year one", description: "Check out your collections from year one", buttonLabel: "Explore Year one", cardImage: yearOnePic },
+    { id: 2, link: "year-two", title: "Year Two", description: "Check out your collections from year two", buttonLabel: "Explore Year two", cardImage: yearTwopic },
+    { id: 3, link: "year-three", title: "Year Three", description: "Checkout your highlights from year three", buttonLabel: "Explore year three", cardImage: yearthreePic },
+    { id: 2, link: "year-four", title: "Year four", description: "Checkout your highlights from year four", buttonLabel: "Explore Year four", cardImage: yearFourPic },
+    { id: 2, link: "final-year", title: "Year five", description: "Checkout your highlights from year five", buttonLabel: "Explore Year five", cardImage: finalYearPic },
 ]
