f6a1a7e12ed53d867f280a6275e14c05640e46a0
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-01-05 19:04:32 +0000

    Updating cards

--- src/Components/Pages/Dashboard/Dashboard.tsx
+++ src/Components/Pages/Dashboard/Dashboard.tsx
@@ -1,19 +1,23 @@
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, HStack, } from "@chakra-ui/react";
 import UnverifiedMessage from "../../Sections/Unverified";
 import CardRenderers from "../../Sections/Cards/CardRenderers";
 import HighlightCarousel from "../../Sections/Slider/Carousel";
+import FileHighlightCard from "../../Sections/Cards/FileHighlightCard";
+
+
 
 
 export default function Dashboard() {
     const isVerified = true
     return (
-        <Box padding={0} height="300px">
+        <Box bg="#FDFDFD" padding={0} height="300px">
             <section>
                 <Box padding={0}>
                     <HighlightCarousel />
                 </Box>
             </section>
             {!isVerified && <UnverifiedMessage />}
+            <FileHighlightCard />
             <section>
                 <HStack align="center" justify="center" p={5} gap={10} wrap="wrap">
                     <CardRenderers />
